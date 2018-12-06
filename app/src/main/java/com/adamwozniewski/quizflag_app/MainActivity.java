7b38b8e3fcd928261fb47e7af2d8cb6ebe50d851
Adam Woźniewski <adam.wozniewski@intive.com>
2018-12-06 14:58:41 +0100

    pierwsze funkcjonalnosci, implementacja MainActivity, bez kilku funkcji

--- app/src/main/java/com/adamwozniewski/quizflag_app/MainActivity.java
+++ app/src/main/java/com/adamwozniewski/quizflag_app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.adamwozniewski.quizflag_app;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +13,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Klucze preferencji
+
+    public static final String CHOICES = "pref_numberofChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+//    Czy aplikacja została uruchomiona na telefonie?
+    private boolean phoneDevice = true;
+
+//    Czy nastąpiła zmiana preferencji
+    private boolean preferencesChanged = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +37,102 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+//        Przypisywanie domyslnych ustawień do obiektu SharedPreferences
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+//        Rejestrowanie obiektu nasłuchującego zmian obiektu SharedPreferences
+        PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(preferencesChangedListener);
+
+        // Pobranie rozmiaru ekranu urządzenia
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // Jezeli rozmiar ekranu jest typowy dla tabletu to:
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+            screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            this.phoneDevice = false;
+        }
+
+        // Jeżeli uruchamiamy aplikację na telefonie to
+        if (this.phoneDevice) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (this.preferencesChanged) {
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateQuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            this.preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Pobranie informacji o orientacji urządzenia
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
 
         return super.onOptionsItemSelected(item);
     }
+
+//    Obiekt nasłuchujący zmian obiektu SharedPreferences
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangedListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            // Użytkownik zmienił ustawienia aplikacji
+            MainActivity.this.preferencesChanged = true;
+
+//            Inicjalizacja obiektu MainActivityFragment
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+//            Instrukcja warunkowa dla rodzaju zmienionych ustawień
+            if (key.equals(MainActivity.CHOICES)) {
+
+//                Aktualizacja liczby wyświetlanych wierszy z przyciskami odpowiedzi
+                quizFragment.updateGuessRows(sharedPreferences);
+
+//                Zresetowanie quizu
+                quizFragment.resetQuiz();
+
+            } else if (key.equals(MainActivity.REGIONS)) {
+//                Pobranie listy wybranych obszarów
+                Set<String> regions = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
+//                Jeżeli wybrano więcej niż jeden obszar
+                if (regions != null && regions.size() > 0) {
+                    quizFragment.updateRegions(sharedPreferences);
+                    quizFragment.resetQuiz();
+                } else {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(MainActivity.REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                }
+
+//                Informowanie użytkownika o restarcie quizu
+                Toast.makeText(MainActivity.this, R.string.reset_quiz, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
